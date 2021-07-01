cacfc2d140781cc0f327965dd3ad720ae9729a3b
mqsaud <mqsaud@gmail.com>
2021-07-01 13:52:30 +0000

    Add the dtails of the members of Torin And Company using for loop

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -11,17 +12,20 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/career")
-def career():
-    return render_template("career.html")
+def Career():
+    return render_template("career.html", page_title="Career" )
 
 
 if __name__ == "__main__":
